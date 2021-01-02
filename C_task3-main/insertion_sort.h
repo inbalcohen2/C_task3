e9bceee9e0e82e534d675344b83a0825f4858c2b
Inbal Cohen <57721728+inbalcohen2@users.noreply.github.com>
2021-01-02 21:36:20 +0200

    Delete insertion_sort.h

--- C_task3-main/insertion_sort.h
+++
@@ -1,9 +0,0 @@
-#ifndef INSERTION_SORT_H
-#define INSERTION_SORT_H
-
-
-void shift_element (int* arr , int i);
-void insertion_sort(int* arr , int len);
-//void f_printArray(int arr[], int n);
-
-#endif //INSERTION_SORT_H
